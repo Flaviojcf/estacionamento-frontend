166358223a2b2d1aae43072a21b1c62f75a96eaa
Flavio <fjcf@poli.br>
2024-01-08 19:21:43 -0300

    refactor: Updated px in footer component

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export function Footer() {
   return (
-    <footer className="flex flex-col text-black-100 mt-5  dark:border-gray-700 transition-colors duration-200">
+    <footer className="flex flex-col text-black-100 mt-5 dark:border-gray-700 transition-colors duration-200">
       <div
         className="flex justify-between items-center flex-wrap mt-10 border-t 
-      border-gray-100 px-6 py-10 sm:px-16 dark:border-gray-700 transition-colors duration-200 gap-2 "
+      border-gray-100 px-24 py-10 sm:px-16 dark:border-gray-700 transition-colors duration-200 gap-2 "
       >
         <Link
           className="dark:text-gray-200 transition-colors duration-200"
