e65d66a422e457c9f74e906365b50b9320644c0d
Flavio <fjcf@poli.br>
2024-01-15 19:39:26 -0300

    feat: Add new custom collors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,6 +36,17 @@ const config: Config = {
       colors: {
         buttonColor: '#7EBC43',
         bgOff: '#F79552',
+        'black-100': '#2B2C35',
+        'primary-blue': {
+          DEFAULT: '#2B59FF',
+          100: '#F5F8FF',
+        },
+        'secondary-orange': '#f79761',
+        'light-white': {
+          DEFAULT: 'rgba(59,60,152,0.03)',
+          100: 'rgba(59,60,152,0.02)',
+        },
+        grey: '#747A88',
       },
     },
   },
